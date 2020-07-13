af8b048a08eb8c76a8bb220265243a256d65c129
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-13 11:40:49 +0800

    Update NormalNpc.h

--- code/Utility/Common/NormalNpc.h
+++ code/Utility/Common/NormalNpc.h
@@ -5,7 +5,7 @@ class NormalNpc : public Npc
 {
 public:
 	NormalNpc();
-	NormalNpc(char* PictureMap, int Name, double Width, double Height, double StartPosX, double StartPosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower);
+	NormalNpc(char* PictureMap, int Name, double Width, double Height, double StartPosX, double StartPosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower, int Number);
 	NormalNpc(const NormalNpc& nn);
 	~NormalNpc();
 	//virtual void Collision();
