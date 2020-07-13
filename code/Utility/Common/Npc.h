8dee6ebddb537fbb3d47ec86e181ce4e61588c47
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-13 11:41:44 +0800

    Update Npc.h

--- code/Utility/Common/Npc.h
+++ code/Utility/Common/Npc.h
@@ -5,7 +5,12 @@ class Npc : public Aircraft
 {
 public:
 	Npc();
-	Npc(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower);
+	Npc(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower, int Number);
 	Npc(const Npc& n);
 	~Npc() {}
+	void SetNumber(int Number);
+	int GetNumber();
+protected:
+	int m_Number;
+	//Npc编号，用于唯一确定Npc
 };
