a05b55bd4e0580b78b3e03530ad1184663de40d9
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-13 11:43:39 +0800

    Update PlayerBullet.h

--- code/Utility/Common/PlayerBullet.h
+++ code/Utility/Common/PlayerBullet.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Bullet.h"
+#include "Player.h"
 
 class PlayerBullet : public Bullet
 {
@@ -8,4 +9,5 @@ public:
 	PlayerBullet(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int ObjectType, bool Release, bool Display);
 	PlayerBullet(const PlayerBullet& pb);
 	~PlayerBullet() {}
+	void SetBulletPosition(const Player& p);
 };
