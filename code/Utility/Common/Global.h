6a8682a8d3dd9047ff409c243670a68d5d4f21f2
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-13 11:39:54 +0800

    Update Global.h

--- code/Utility/Common/Global.h
+++ code/Utility/Common/Global.h
@@ -23,6 +23,11 @@
 #define NONEWIDTH -1
 #define NONEHEIGHT -1
 
+//scene size setting
+//need further changes
+#define SCENEWIDTH 1000;
+#define SCENEHEIGHT 2000;
+
 //game parameter constant values
 #define NORMALNPCSPEED 1
 #define BOSSNPCSPEED 2
@@ -39,6 +44,14 @@
 #define BOSSNPCFIREPOWER 2
 #define PLAYERFIREPOWER 3
 #define NONEFIREPOWER -1
+
+//coordinate the spatial relationship between player aircraft and its bullets
+#define PLAYERBULLETWIDTHOFFSET 10.0
+#define PLAYERBULLETHEIGHTOFFSET 10.0
+#define NORMALNPCBULLETWIDTHOFFSET 10.0
+#define NORMALNPCBULLETHEIGHTOFFSET 10.0
+#define BOSSNPCBULLETWIDTHOFFSET 10.0
+#define BOSSNPCBULLETHEIGHTOFFSET 10.0
 
 //Data definition
 typedef enum { Pos, Neg, Neither } MOVEDIRECTION;
