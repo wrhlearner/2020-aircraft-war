1d5ad1f8316631443a36d4f56f678891e5a5d3ed
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-13 11:41:26 +0800

    Update Npc.cpp

--- code/Utility/Common/Npc.cpp
+++ code/Utility/Common/Npc.cpp
@@ -1,7 +1,10 @@
 #include "Npc.h"
 
 Npc::Npc() {}
-Npc::Npc(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower)Aircraft(PictureMap, Name, Width, Height, PosX, PosY, Speed, Direction, Exist, Blood, FirePower) {}
+Npc::Npc(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower, int Number):Aircraft(PictureMap, Name, Width, Height, PosX, PosY, Speed, Direction, Exist, Blood, FirePower) 
+{
+	m_Number = Number;
+}
 Npc::Npc(const Npc& n)
 {
 	m_Name = n.m_Name;
@@ -15,4 +18,7 @@ Npc::Npc(const Npc& n)
 	m_PictureMap = n.m_PictureMap;
 	m_Blood = n.m_Blood;
 	m_FirePower = n.m_FirePower;
+	m_Number = n.m_Number;
 }
+void Npc::SetNumber(int Number) { m_Number = Number; }
+int Npc::GetNumber() { return m_Number; }
