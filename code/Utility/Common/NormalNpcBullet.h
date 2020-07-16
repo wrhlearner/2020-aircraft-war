f86019f949ddd48de9fbbbcb2d41a298ae708cbb
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-16 08:55:20 +0800

    Update NormalNpcBullet.h

--- code/Utility/Common/NormalNpcBullet.h
+++ code/Utility/Common/NormalNpcBullet.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef NORMALNPCBULLET_H
+#define NORMALNPCBULLET_H
+
 #include "NpcBullet.h"
 #include "NormalNpc.h"
 
@@ -9,11 +11,13 @@ public:
 	NormalNpcBullet(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int ObjectType, bool Release, bool Display);
 	NormalNpcBullet(const NormalNpcBullet& nb);
 	~NormalNpcBullet();
-	void SetNormalNpcBulletPosition(const NormalNpc& nn);
-	void SetNormalNpcNumber(const NormalNpc& nn);
+	void SetNormalNpcBulletPosition(NormalNpc& nn);
+	void SetNormalNpcNumber(NormalNpc& nn);
 	void SetNormalNpcNumber(int NormalNpcNumber);
 	int GetNormalNpcNumber() const;
 protected:
 	int m_NormalNpcNumber;
 	//将子弹与Npc绑定，便于在不同时间获得子弹的位置
 };
+
+#endif // !NORMALNPCBULLET_H
