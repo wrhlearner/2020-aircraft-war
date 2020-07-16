102997958b424e0b608e6b59180b557781fc06a9
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-16 09:01:30 +0800

    Update RandAlg.h

--- code/Utility/Common/RandAlg.h
+++ code/Utility/Common/RandAlg.h
@@ -1,4 +1,9 @@
-#pragma once
-#include<stdio.h>
+#ifndef RANDALG_H
+#define RANDALG_H
+
+#include<stdlib.h>
+#include "Global.h"
 
 int GetRandNum(int UpperBound, int LowerBound, int Seed);
+
+#endif // !RANDALG_H
