dec64c8e0c72d6d43aa6148f34bcae921db1fad5
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-16 08:54:45 +0800

    Update NormalNpc.h

--- code/Utility/Common/NormalNpc.h
+++ code/Utility/Common/NormalNpc.h
@@ -1,7 +1,12 @@
-#pragma once
+#ifndef NORMALNPC_H
+#define NORMALNPC_H
+
 #include "Npc.h"
 #include "Player.h"
 #include "GenerateBullet.h"
+
+class Player;
+class GenerateBullet;
 
 class NormalNpc : public Npc
 {
@@ -9,8 +14,10 @@ public:
 	NormalNpc();
 	NormalNpc(char* PictureMap, int Name, double Width, double Height, double StartPosX, double StartPosY, double Speed, MOVEDIRECTION Direction, bool Exist, int Blood, int FirePower, int Number);
 	NormalNpc(const NormalNpc& nn);
-	~NormalNpc();
-	
+	~NormalNpc() {}
+
 	int Collision(Player& TempPlayer, GenerateBullet::PlayerBullets pb);
 	GenerateBullet::NormalNpcBullets LoadBullet(GenerateBullet::NormalNpcBullets nnb);
 };
+
+#endif // !NORMALNPC_H
