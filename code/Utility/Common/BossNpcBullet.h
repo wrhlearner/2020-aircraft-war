3fd87ff7fb84930d5d1ae02ee25afd919a108b8a
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-16 08:47:43 +0800

    Update BossNpcBullet.h

--- code/Utility/Common/BossNpcBullet.h
+++ code/Utility/Common/BossNpcBullet.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef BOSSNPCBULLET_H
+#define BOSSNPCBULLET_H
+
 #include "NpcBullet.h"
 #include "BossNpc.h"
 
@@ -9,11 +11,13 @@ public:
 	BossNpcBullet(char* PictureMap, int Name, double Width, double Height, double PosX, double PosY, double Speed, MOVEDIRECTION Direction, bool Exist, int ObjectType, bool Release, bool Display);
 	BossNpcBullet(const BossNpcBullet& bn);
 	~BossNpcBullet();
-	void SetBossNpcBulletPosition(const BossNpc& bn);
-	void SetBossNpcNumber(const BossNpc& bn);
+	void SetBossNpcBulletPosition(BossNpc& bn);
+	void SetBossNpcNumber(BossNpc& bn);
 	void SetBossNpcNumber(int BossNpcNumber);
 	int GetBossNpcNumber() const;
 protected:
 	int m_BossNpcNumber;
 	//将子弹与Npc绑定，便于在不同时间获得子弹的位置
 };
+
+#endif // !BOSSNPCBULLET_H
