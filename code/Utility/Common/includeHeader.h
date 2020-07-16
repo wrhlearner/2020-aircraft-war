15f3f42ab3153208c02dfde29eecad1c719842cb
wrhlearner <52666636+wrhlearner@users.noreply.github.com>
2020-07-16 08:50:46 +0800

    Add files via upload

---
+++ code/Utility/Common/includeHeader.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDEHEADER_H
+#define INCLUDEHEADER_H
+
+#include "Global.h"
+#include "RandAlg.h"
+#include "FlyingObject.h"
+#include "Aircraft.h"
+#include "Bullet.h"
+#include "Npc.h"
+#include "NpcBullet.h"
+#include "NormalNpc.h"
+#include "BossNpc.h"
+#include "GenerateNpc.h"
+#include "NormalNpcBullet.h"
+#include "BossNpcBullet.h"
+#include "Player.h"
+#include "PlayerBullet.h"
+#include "GenerateBullet.h"
+
+#endif // !INCLUDEHEADER_H
