3634d0bc569628cda6fecba0b280d714bea41c4c
AthlonXP2000 <51393131+AthlonXP2000@users.noreply.github.com>
2020-07-19 04:27:45 +0800

    Add files via upload

--- code/Aircraft-war/Window/MainWindow.cpp
+++ code/Aircraft-war/Window/MainWindow.cpp
@@ -2,6 +2,8 @@
 #include "MainWindow.h"
 #include "./ui_mainwindow.h"
 #include <Utility\Common\NormalNpc.h>
+#include <QKeyEvent>
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,33 +12,66 @@ MainWindow::MainWindow(QWidget *parent)
 	m_sinkCommand(std::make_shared<MainWindowCommandSink>(this))
 {
     ui->setupUi(this);
+    initScene();
 }
 
-
-void MainWindow::set_QueryCommand(const std::shared_ptr<ICommandBase>& cmd) throw()
+void MainWindow::set_rankadd_command(const std::shared_ptr<ICommandBase>& cmd) throw()
 {
-	m_cmdQuery = cmd;
+	m_cmdrank = cmd;
 }
 
-MainWindow& get_WindowPointer() throw()
+void MainWindow::set_restart_command(const std::shared_ptr<ICommandBase>& cmd) throw()
 {
-    MainWindow& p_mw = this;
-    return p_mw;
+	m_cmdrst = cmd;
 }
 
-std::shared_ptr<IPropertyNotification> MainWindow::get_PropertySink() throw()
+void MainWindow::set_key_command(const std::shared_ptr<ICommandBase>& cmd) throw()
 {
-	return std::static_pointer_cast<IPropertyNotification>(m_sinkProperty);
+	m_cmdkey = cmd;
 }
-std::shared_ptr<ICommandNotification> MainWindow::get_CommandSink() throw()
+
+void MainWindow::set_mouse_command(const std::shared_ptr<ICommandBase>& cmd) throw()
 {
-	return std::static_pointer_cast<ICommandNotification>(m_sinkCommand);
+	m_cmdmouse = cmd;
+}
+
+std::shared_ptr<IPropertyNotification> MainWindow::get_PlanePropertySink() throw()
+{
+	return std::static_pointer_cast<IPropertyNotification>(m_sinkPropPlane);
+}
+
+std::shared_ptr<IPropertyNotification> MainWindow::get_BulletPropertySink() throw()
+{
+	return std::static_pointer_cast<IPropertyNotification>(m_sinkPropBullet);
+}
+
+std::shared_ptr<ICommandNotification> MainWindow::get_RankCommandSink() throw()
+{
+	return std::static_pointer_cast<ICommandNotification>(m_sinkCommRank);
+}
+
+std::shared_ptr<ICommandNotification> MainWindow::get_RstCommandSink() throw()
+{
+	return std::static_pointer_cast<ICommandNotification>(m_sinkCommRst);
+}
+
+std::shared_ptr<ICommandNotification> MainWindow::get_KeyCommandSink() throw()
+{
+	return std::static_pointer_cast<ICommandNotification>(m_sinkCommKey);
+}
+
+std::shared_ptr<ICommandNotification> MainWindow::get_MouseCommandSink() throw()
+{
+	return std::static_pointer_cast<ICommandNotification>(m_sinkCommMouse);
 }
 
 void MainWindow::initScene(){
     setWindowTitle(GAME_TITLE);
     setFixedSize(GAME_WIDTH,GAME_HEIGHT);
     setWindowIcon(QIcon(GAME_ICON));
+    m_GV.setScene(&m_GS);
+    Q_Plane.getGS(std::static_pointer_cast<GraphicsScene>(*m_GS));
+    Q_Bullet.getGS(std::static_pointer_cast<GraphicsScene>(*m_GS));
 } 
 
 MainWindow::~MainWindow()
@@ -44,9 +79,97 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void GraphicsScene::Update(std::list<NormalNpc>& Prop, int PropType)
+void MainWindow::mouseMoveEvent(QMouseEvent* event){
+    int x = event->x;
+    int y = event->y;
+    m_cmdmouse->SetParameter(x,y,"Move");
+    m_cmdmouse->exec();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
 {
-    if(PropType == 0){
+    if(event->button() == Qt::LeftButton)
+   {
+        m_cmdmouse->SetParameter(1,0,"Press");
+        m_cmdmouse->exec;
+    }
+    else if(event->button() == Qt::RightButton)
+   {
+        m_cmdmouse->SetParameter(0,1,"Press");
+        m_cmdmouse->exec;
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+   {
+        m_cmdmouse->SetParameter(1,0,"Release");
+        m_cmdmouse->exec;
+    }
+    else if(event->button() == Qt::RightButton)
+   {
+        m_cmdmouse->SetParameter(0,1,"Release");
+        m_cmdmouse->exec;
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *ev)
+{
+　　if (ev->key() == Qt::Key_Up)
+　　{
+        //qDebug() << "press key up";
+        m_cmdkb->SetParameter("Key_Up");
+        m_cmdkb->exec();
+        return;
+　　}
+　　if (ev->key() == Qt::Key_Down)
+　　{
+        //qDebug() << "press key down";
+        m_cmdkb->SetParameter("Key_Down");
+        m_cmdkb->exec();
+        return;
+　　}
+　　if (ev->key() == Qt::Key_Left)
+　　{
+        //qDebug() << "press key left";
+        m_cmdkb->SetParameter("Key_Left");
+        m_cmdkb->exec();
+        return;
+　　}
+　　if (ev->key() == Qt::Key_Right)
+　　{
+        //qDebug() << "press key right";
+        m_cmdkb->SetParameter("Key_Right");
+        m_cmdkb->exec();
+        return;
+　　}
+    if (ev->key() == Qt::Key_Z)
+    {
+        m_cmdkb->SetParameter("Key_Z");
+        m_cmdkb->exec();
+        //qDebug() << "press shot key";
+        return;
+    }
+    if (ev->key() == Qt::Key_X)
+    {
+        m_cmdkb->SetParameter("Key_X");
+        m_cmdkb->exec();
+        //qDebug() << "press bomb key";
+        return;
+    }
+
+    if (ev->key() == Qt::Key_R)
+    {
+        m_cmdrst->exec();
+        //qDebug() << "press reset key";
+        return;
+    }
+}
+
+void GraphicsScene::Update(std::list<FlyingObject>& Prop, int PropType)
+{
+/*     if(PropType == 0){
         std::list<VisualObject>::iterator Npc_VO_iter = VO_Npc.begin();
         while (Npc_VO_iter != Prop.end()){
             ~Npc_VO_iter;
@@ -60,5 +183,6 @@ void GraphicsScene::Update(std::list<NormalNpc>& Prop, int PropType)
             ~New_NPC();
         }
         m_GS.show();
-    }
+    } */
+    render();
 }
--- code/Aircraft-war/Window/MainWindow.h
+++ code/Aircraft-war/Window/MainWindow.h
@@ -4,12 +4,15 @@
 #include <QMainWindow>
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 #include <VisualObject.h>
-#include <Socket.h>
+//#include <Socket.h>
 #include <Aircraft-war\window\sink\MainWindowCommandSink.h>
 #include <Aircraft-war\window\sink\MainWindowPropertySink.h>
 #include <Aircraft-war\Socket.h>
 #include <Aircraft-war\view\VisualObject.h>
+#include <Utility\ViewModel\planeviewmodel.h>
+#include <Utility\ViewModel\bulletviewmodel.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,25 +25,29 @@ class MainWindow : public QMainWindow
 public:
     GraphicsView m_GV;
     GraphicsScene m_GS;
-    std::list<VisualObject> VO_Npc;//, VO_Boss, VO_Bullet, VO_Item;
-    VisualObject VO_Player;
+    VisualObjects Q_Plane, Q_Bullet;//, Q_Item;
 
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
     void initScene();
-
-    void set_keyCommand(const int& keyv) throw();
-    void set_QueryCommand(const std::shared_ptr<ICommandBase>& cmd) throw();
-    MainWindow& get_WindowPointer() throw();
+    void keyPressEvent(QKeyEvent *ev)
+    void mouseMoveEvent(QMouseEvent* event)
+    void mousePressEvent(QMouseEvent* event)
+    void mouseReleaseEvent(QMouseEvent* event)
+    void set_mouse_command(const std::shared_ptr<ICommandBase>& cmd) throw();
+    void set_key_command(const std::shared_ptr<ICommandBase>& cmd) throw();
+    void set_rankadd_command(const std::shared_ptr<ICommandBase>& cmd) throw();
+    void set_restart_command(const std::shared_ptr<ICommandBase>& cmd) throw();
 	std::shared_ptr<IPropertyNotification> get_PropertySink() throw();
 	std::shared_ptr<ICommandNotification> get_CommandSink() throw();
 
 private:
     Ui::MainWindow *ui;
-    std::shared_ptr<ICommandBase> m_cmdQuery;
-	std::shared_ptr<MainWindowPropertySink> m_sinkProperty;
-	std::shared_ptr<MainWindowCommandSink> m_sinkCommand;
+    std::shared_ptr<ICommandBase> m_cmdrank, m_cmdrst, m_cmdkey, m_cmdmouse;
+	std::shared_ptr<MainWindowPropertySink> m_sinkPropPlane, m_sinkPropBullet;
+    std::shared_ptr<MainWindowGameStateSink> m_sinkPropGameState;
+	std::shared_ptr<MainWindowCommandSink> m_sinkCommRank, m_sinkCommRst, m_sinkCommKey, m_sinkCommMouse;
 };
 
 class GraphicsScene : public QGraphicsScene
@@ -50,9 +57,6 @@ class GraphicsScene : public QGraphicsScene
 public:
     GraphicsScene(qreal x, qreal y, qreal width, qreal height, QObject* parent = nullptr);
     ~GraphicsScene();
-
-    void Update(std::list<NormalNpc>& Prop, int PropType);
-
 };
 
 class GraphicsView : public QGraphicsView
